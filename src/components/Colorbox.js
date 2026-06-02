11d497a39f6947b0a89a358605b38052ce4bacb5
ysjeondev <hpc9909@gmail.com>
2026-06-02 18:59:01 +0900

    260602 페이지 410까지 완료 15장 완료

--- src/components/Colorbox.js
+++ src/components/Colorbox.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import {ColorConsumer} from '../contexts/colors';
+import React, {useContext} from 'react';
+import ColorContext from '../contexts/colors';
 
 const ColorBox = () => {
+    const { state } = useContext(ColorContext);
     return (
-        <ColorConsumer>
-            {({state}) => (
-                <>
-                <div 
-                    style={{
-                        width:'64px',
-                        height:'64px',
-                        background:state.color
-                    }}
-                />
-                <div
-                    style={{
-                        width:'32px',
-                        height:'32px',
-                        background:state.subColor
-                    }}
-                />
-                </>
-            )}
-        </ColorConsumer>
+        <>
+            <div
+                style={{
+                    width:'64px',
+                    height:'64px',
+                    background:state.color
+                }}
+            />
+            <div
+                style={{
+                    width:'32px',
+                    height:'32px',
+                    background:state.subColor
+                }}
+            />
+        </>
     )
 }
 
--- src/components/SelectColors.js
+++ src/components/SelectColors.js
@@ -1,34 +1,44 @@
-import { ColorConsumer } from "../contexts/colors";
+import React, { Component } from 'react';
+import ColorContext from '../contexts/colors';
+
 const colors = ['red','orange','yellow','green','blue','indigo','violet'];
-const SelectColors = () => {
-    return (
+
+class SelectColors extends Component {
+    static contextType = ColorContext;
+
+    handleSetColor= color => {
+        this.context.actions.setColor(color);
+    }
+
+    handleSetSubcolor = subcolor => {
+        this.context.actions.setSubColor(subcolor);
+    };
+
+    render(){
+        return(
         <div>
             <h2>색상을 선택하세요</h2>
-            <ColorConsumer>
-                {({ actions }) => ( 
-                    <div style={{ display:'flex' }}>
-                        {colors.map(color => (
-                            <div
-                                key={color}
-                                style={{
-                                backgroundColor: color,
-                                width:'50px',
-                                height:'50px',
-                                cursor:'pointer',
-                                marginRight:'10px'
+            <div style={{ display:'flex' }}>
+                { colors.map( color => (
+                    <div 
+                        key={color}
+                        style={{
+                            backgroundColor: color,
+                            width:'50px',
+                            height:'50px',
+                            cursor:'pointer'
                         }}
-                        onClick={() => actions.setColor(color)}
+                        onClick={() => this.handleSetSubcolor(color)}
                         onContextMenu={ e => {
                             e.preventDefault();
-                            actions.SelectColors(color);
+                            this.handleSetSubcolor(color);
                         }}
                     />
                 ))}
-                </div>
-                )}
-            </ColorConsumer>
+            </div>
             <hr />
         </div>
-    );
+        );
+    }
 }
 export default SelectColors;
